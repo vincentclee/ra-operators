3ea537c3ab1f246e0089f01a6dbf4cfbff3a95a2
Collin Watts <locke@uga.edu>
2014-06-12 22:59:14 -0400

    Defined test blocks

---
+++ src/com/csci4370/test/TableJoinTest.java
@@ -0,0 +1,14 @@
+package com.csci4370.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TableJoinTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
